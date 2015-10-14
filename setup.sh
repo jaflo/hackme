5f4e9dfd761e9ebd05e0fd8795b70b18f623dcc2
Florian <imjaflo@gmail.com>
2015-10-14 13:48:33 -0500

    Even better DB instructions

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ cd public_html
 git clone https://github.com/jaflo/hackme.git
 mv hackme/* ./
 rm -rf hackme
-sudo mysql -u nitrous -e "CREATE DATABASE `hackthis`;"
+sudo mysql -u nitrous -e "CREATE DATABASE hackthis"
 sudo mysql -u nitrous hackthis < init.sql
