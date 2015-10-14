5f7f2994669252c5102ca159b3c2729a5ca48dd0
Florian <imjaflo@gmail.com>
2015-10-14 13:46:25 -0500

    Changed DB instructions, more comprehensive setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
-mysql -u root -p "" < init.sql
+# For nitrous.io
+
+cd public_html
+git clone https://github.com/jaflo/hackme.git
+mv hackme/* ./
+rm -rf hackme
+sudo mysql -u nitrous -e "CREATE DATABASE `hackthis`;"
+sudo mysql -u nitrous hackthis < init.sql
